3b80ec7d4e4100d1945e278e96a94971b4ee985a
huang puguang <puguang_huang@fpi-inc.com>
2019-09-20 16:33:09 +0800

    fix: 日志绑定

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import logging
 # 路由引入
 import router
 # 新建实例
@@ -9,3 +10,9 @@ app.register_blueprint(router.template, url_prefix='/template')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
+
+if __name__ != '__main__':
+    # 如果不是直接运行，则将日志输出到 gunicorn 中
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
